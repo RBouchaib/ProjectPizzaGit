4068181cdea05e187a7bd572610b423e025b3a74
Rabab Bouchaib <rabab.bouchaib13@hotmail.com>
2021-07-31 00:29:35 +0200

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'hawaienne': {
+        'ingredients': {'mozzarella': 3, 'mushroom': 5, 'onion': 4, 'ham': 3},
+        'price': 12,
+    },
 }
